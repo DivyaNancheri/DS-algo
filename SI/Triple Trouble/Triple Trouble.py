b943761d296e408bc0a6e4f189ca376f441b78b9
nancheridivya <nancheridivya@gmail.com>
2020-11-26 21:30:26 +0530

    SI triple trouble

--- SI/Triple Trouble/Triple Trouble.py
+++ SI/Triple Trouble/Triple Trouble.py
@@ -7,15 +7,21 @@ for i in range(t):
     n=int(input())
     a = [int(i) for i in input().split()]
     count=0
-    for i in range(0,len(a)):
+    ans=0
+    for i in range(0,4):
         count=0
-        print("a[i]=",a[i],end=' ')
         for j in range(0,len(a)):
-            if(a[i]==a[j] and i!=j):
+            # print('a[',j,']=',a[j],end=' ')
+            # print('i',i,end=' ')
+            if(((a[j]>>i)&1)==1):
+                # print("inisde if,i=",i, end=' ')
                 count+=1
-        print("count=",count)
-        if(count==0):
-            print(a[i])
-            break
+                # print('count=',count)
+        # print('count=',count)
+        if(count%3 !=0):
+            # print("inisde count if,i=",i)
+            ans=ans+ (1<<i)
+            # print("inisde count if,ans=",ans)
+    print(ans)
 
     
