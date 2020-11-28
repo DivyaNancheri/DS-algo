6d8da6c93453cb0700759f977c1cf80c3e07f23b
nancheridivya <nancheridivya@gmail.com>
2020-11-28 23:49:42 +0530

    test

--- testing/testing.py
+++ testing/testing.py
@@ -2,28 +2,15 @@ import sys
 sys.stdout = open('testing/output.txt', 'w')
 sys.stdin = open('testing/input.txt', 'r')
 
-# cook your dish here
-tc = int(input())
- 
-while tc>0:
-    
+t=int(input())
+for i in range(t):
     n=int(input())
     a = [int(i) for i in input().split()]
-    if(n==1):
-    	print(a[0])
-    elif(n==2):
-    	print(max(a[0], a[1]))
-    elif(n==3):
-    	a.sort() 
-    	print(max(a[0]+a[1], a[2]))
-    else:
-    	a.sort() 
-    	b1=max(a[0]+a[3],a[1]+a[2])
-    	b2=max(a[3],a[1]+a[0]+a[2])
-    	print(min(b1,b2))
+    l=a[0]
+    for i in range(1,n):
+        l=l+a[i]
+    print(l)
     
-    tc-=1
-        
      
      
             
